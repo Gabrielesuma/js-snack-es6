c49fb086f70782a6e0bd54c87684f0ae41e7aa7a
Gabrielesuma <gabrielesuma139@gmail.com>
2024-03-22 16:05:41 +0100

    snack 2

--- js/script.js
+++ js/script.js
@@ -6,6 +6,59 @@ const vip = tavoloVip.map((el) => {
 console.log(vip);
 
 
+var classe = [
+    {
+        id: 213,
+        name: 'Marco della Rovere',
+        grade: 78
+    },
+    {
+        id: 110,
+        name: 'Paola Cortellessa',
+        grade: 96
+    },
+    {
+        id: 250,
+        name: 'Andrea Mantegna',
+        grade: 48
+    },
+    {
+        id: 145,
+        name: 'Gaia Borromini',
+        grade: 74
+    },
+    {
+        id: 196,
+        name: 'Luigi Grimaldello',
+        grade: 68
+    },
+    {
+        id: 102,
+        name: 'Piero della Francesca',
+        grade: 50
+    },
+    {
+        id: 120,
+        name: 'Francesca da Polenta',
+        grade: 84
+    }
+];
+
+const newClass = classe.map((el) => {
+    return el.name.toUpperCase();
+})
+console.log(newClass);
+
+const over70 = classe.filter((el) => {
+    return el.grade > 70;
+})
+console.log(over70);
+
+const gradeId = classe.filter((el) => {
+    return el.grade > 70 && el.id > 120;
+})
+console.log(gradeId);
+
 
 function genRandom(min, max){
     return Math.floor(Math.random() * max - min + 1) + min;
